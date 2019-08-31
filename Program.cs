b0b27e335528592499025854315657500c25709e
JustinHartman407 <jdhartman3@mail.usf.edu>
2019-08-31 13:10:29 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ISM4300Hello2019Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is a test");
+        }
+    }
+}
